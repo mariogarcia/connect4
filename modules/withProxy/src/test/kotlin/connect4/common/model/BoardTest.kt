4b6aecc62f6f8ec9df6bbd166311f6524d938e24
Mario Garcia <mario.ggar@gmail.com>
2021-11-04 22:19:30 +0100

    feat(board): add vertical and less than 4 subsequent cells tests

--- modules/withProxy/src/test/kotlin/connect4/common/model/BoardTest.kt
+++ modules/withProxy/src/test/kotlin/connect4/common/model/BoardTest.kt
@@ -47,10 +47,26 @@ internal class BoardTest {
         -, -, -, -, -, -
         -, -, -, -, -, -
         -, -, -, -, -, -
-        -, -, -, -, -, -        
+        -, -, -, -, -, -
     """])
     fun `given a board, when there is a row with 4 subsequent cells with same color, there is connect4`(
         @ConvertWith(BoardValueSourceConverter::class) board: Board
+    ) {
+        assertTrue(board.isConnect4())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["""
+        -, -, -, -, -, -
+        -, -, -, -, -, -
+        -, -, R, -, -, -
+        -, -, R, -, -, -
+        -, -, R, -, -, -
+        -, -, R, -, -, -
+        -, -, -, -, -, -
+    """])
+    fun `given a board, when there is a column with 4 subsequent cells with same color, there is connect4`(
+            @ConvertWith(BoardValueSourceConverter::class) board: Board
     ) {
         assertTrue(board.isConnect4())
     }
@@ -79,9 +95,25 @@ internal class BoardTest {
         -, -, R, -, -, -
         -, -, -, -, -, -
         -, -, -, -, -, -
-        -, -, -, -, -, -        
+        -, -, -, -, -, -
     """])
     fun `given a board, when there is an inverse diagonal cells with same color, there is connect4`(
+            @ConvertWith(BoardValueSourceConverter::class) board: Board
+    ) {
+        assertTrue(board.isConnect4())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["""
+        -, -, -, -, -, =
+        -, -, -, -, -, -
+        -, -, -, -, -, -
+        -, -, R, R, R, -
+        -, -, -, -, -, -
+        -, -, -, -, -, -
+        -, -, -, -, -, -
+    """])
+    fun `given a board, when there are less than 4 subsequent cells with same color, there is NOT connect4`(
             @ConvertWith(BoardValueSourceConverter::class) board: Board
     ) {
         assertTrue(board.isConnect4())
