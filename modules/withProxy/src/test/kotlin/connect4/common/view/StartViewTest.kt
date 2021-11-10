8251df84509ebf9c51accb50d6170c0801a63527
Mario Garcia <mario.ggar@gmail.com>
2021-11-10 11:57:07 +0100

    fix(tests): aggregate tests that are unnecessarily spread

--- modules/withProxy/src/test/kotlin/connect4/common/view/StartViewTest.kt
+++ modules/withProxy/src/test/kotlin/connect4/common/view/StartViewTest.kt
@@ -7,8 +7,7 @@ import connect4.test.View
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 
 @View
 class StartViewTest: MockInitializer() {
@@ -22,33 +21,16 @@ class StartViewTest: MockInitializer() {
     private lateinit var view: StartView
 
     @Test
-    fun `when start view is shown, controller is reset`() {
+    fun `when start view is shown, a certain interactions are expected`() {
         view.show(controller, console)
-        verify(controller, times(1)).reset()
-    }
 
-    @Test
-    fun `when start view is shown, console is clear`() {
-        view.show(controller, console)
-        verify(console, times(1)).clear()
-    }
-
-    @Test
-    fun `when start view is shown, console says hi`() {
-        view.show(controller, console)
-        verify(console, times(1)).writeLn(anyObject())
-    }
-
-    @Test
-    fun `when start view is shown, console shows the game table`() {
-        view.show(controller, console)
-        verify(controller, times(1)).getBoardColors()
-        verify(console, times(1)).showTable(anyObject())
-    }
-
-    @Test
-    fun `when start view is shown, controller asks for the next state`() {
-        view.show(controller, console)
-        verify(controller, times(1)).nextState()
+        with(inOrder(console, controller)) {
+            verify(controller, times(1)).reset()
+            verify(console, times(1)).clear()
+            verify(console, times(1)).writeLn(anyObject())
+            verify(controller, times(1)).getBoardColors()
+            verify(console, times(1)).showTable(anyObject())
+            verify(controller, times(1)).nextState()
+        }
     }
 }
